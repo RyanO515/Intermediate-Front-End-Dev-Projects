b48e43dcfa7c96f6b6cfa58823cb940659999532
Ryan <ROliver@zghealth.com>
2016-05-02 13:53:06 -0400

    cleaned up code, comments, tab funct

--- Twitch API/app.js
+++ Twitch API/app.js
@@ -1,3 +1,5 @@
+
+// gather all stream and API information in variables
 var streamUrl = 'https://api.twitch.tv/kraken/streams/';
 var channelUrl = 'https://api.twitch.tv/kraken/channels/';
 var streams = ["ESL_LOL", "freecodecamp", "ceh9", "agromon26", "B0aty", "Nightblue3", "imaqtpie", "Reckful","RobotCaleb","Peeve","noobs2ninjas","rxnexus"]; 
@@ -5,24 +7,25 @@ var streams = ["ESL_LOL", "freecodecamp", "ceh9", "agromon26", "B0aty", "Nightbl
 
 function callStreams () {  
 
-  
- 
   streams.forEach(function (streamer) {
-
+      // send API request
       $.getJSON(streamUrl + streamer, function (data) {
         console.log(data);
+        //defaults
         var streamHTML = "";
         var unknownIcon = 'http://www.marketing-professionnel.fr/wp-content/uploads/2011/09/donnees-off-line-on-line.jpg';
         var userURL = "http://www.twitch.tv/" + streamer;
 
 
-        
+        // offline stream
         if (data.stream === null || data.stream === undefined) {
           streamHTML += "<div class='stream offline'>";
           streamHTML += "<img src='" + unknownIcon + "' class='img-rounded' alt='logo' />";
           streamHTML += "<a href='" + userURL + "'>" + streamer + "</a>";
           streamHTML += "<span class='status-info pull-right'>Offline</span>";
-        } else {
+        } 
+        // success
+        else {
           streamHTML += "<div class='stream online'>";
           streamHTML += "<img src='" + data.stream.channel.logo + "' class='img-rounded' alt='logo'/>";
           streamHTML += "<a href='" + data.stream.channel.url + "'>" + data.stream.channel.display_name + "</a>";
@@ -38,17 +41,12 @@ function callStreams () {
  };   //function callstreams
 
 
-
-
-
-
-
-
 $(document).ready(function () {
   
   callStreams();
 
-
+  // all streams called, minor functionality
+  // for active and offline tabs
   $(".status-pill").on("click", function() {
     $(".status-pill").removeClass('active');
     $(this).addClass('active');
